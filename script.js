f7c75a19ee2498261c3c4249c691bae868554907
Zero Liu <zerolxy@gmail.com>
2020-10-06 16:36:33 -0700

    Update UI

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const TEST_DB_NAME = 'test-database';
+const NOT_CALLED = 'not called';
+const ABORT_ERROR = 'AbortError';
+const TRANSACTION_ERROR = 'TransactionError'
+
 const testCases = {
   addSameKeyTwice: {
-    expect: {
-      errorEventTarget: 'IDBRequest',
-      errorTransactionError: '',
-      abortEventTarget: 'IDBTransaction',
-      abortTransactionError:
-          'ConstraintError: Key already exists in the object store.',
-    },
+    description: 'Bad requests - Add the same key twice',
+    expected: TRANSACTION_ERROR,
     upgrade: (db) => {
       db.createObjectStore('entries');
     },
@@ -20,13 +19,9 @@ const testCases = {
     }
   },
   addUniqueIndexTwice: {
-    expect: {
-      errorEventTarget: 'IDBRequest',
-      errorTransactionError: '',
-      abortEventTarget: 'IDBTransaction',
-      abortTransactionError:
-          `ConstraintError: Unable to add key to index 'name': at least one key does not satisfy the uniqueness requirements.`,
-    },
+    description:
+        'Bad requests - Put the same index key when the key has a uniqueness constraint',
+    expected: TRANSACTION_ERROR,
     upgrade: (db) => {
       const store = db.createObjectStore('entries');
       store.createIndex('name', 'name', {unique: true});
@@ -36,6 +31,53 @@ const testCases = {
       const store = tx.objectStore('entries');
       store.add({name: 'foo'}, 1);
       store.add({name: 'foo'}, 2);
+      return tx;
+    }
+  },
+  explicitAbort: {
+    description: 'An explicit abort() call',
+    expected: ABORT_ERROR,
+    upgrade: (db) => {
+      db.createObjectStore('entries');
+    },
+    test: (db) => {
+      const tx = db.transaction('entries', 'readwrite');
+      const store = tx.objectStore('entries');
+      store.add('foo', 1);
+      tx.abort();
+      return tx;
+    }
+  },
+  uncaughtExceptionInSuccessRequest: {
+    description: `An uncaught exception in the request's success handler`,
+    expected: TRANSACTION_ERROR,
+    upgrade: (db) => {
+      db.createObjectStore('entries');
+    },
+    test: (db) => {
+      const tx = db.transaction('entries', 'readwrite');
+      const store = tx.objectStore('entries');
+      const request = store.add('foo', 1);
+      request.addEventListener('success', () => {
+        throw new Error('throw in request success handler');
+      });
+      return tx;
+    }
+  },
+  uncaughtExceptionInErrorRequest: {
+    description: `An uncaught exception in the request's error handler`,
+    expected: TRANSACTION_ERROR,
+    upgrade: (db) => {
+      db.createObjectStore('entries');
+    },
+    test: (db) => {
+      const tx = db.transaction('entries', 'readwrite');
+      const store = tx.objectStore('entries');
+      store.add('foo', 1);
+      const request = store.add('bar', 1);
+      request.addEventListener('error', () => {
+        throw new Error('throw in request error handler');
+      });
       return tx;
     }
   },
@@ -50,22 +92,41 @@ const errorTransactionErrorEl =
     document.querySelector('.error-transaction-error');
 const abortTransactionErrorEl =
     document.querySelector('.abort-transaction-error');
+const errorEventErrorEl = document.querySelector('.error-event-error');
 const testResultStatusEl = document.querySelector('.test-result-status');
 
-const testButtons = document.querySelectorAll('.test-btn');
-testButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    runTest(btn.name);
-  });
-});
-
+generateTestCaseUI();
 userAgentEl.textContent = navigator.userAgent;
+
+
+function generateTestCaseUI() {
+  const container = document.querySelector('.test-cases');
+  for (const key of Object.keys(testCases)) {
+    const testCase = testCases[key];
+    const el = document.createElement('div');
+    el.className = 'test-case';
+    el.innerHTML = `
+      <button class="test-btn">TEST</button>
+      ${
+        testCase
+            .description} (Expected: <span class="test-result-status result">${
+        testCase.expected}</span>)
+    `;
+    const button = el.querySelector('.test-btn');
+    button.addEventListener('click', () => {
+      runTest(key);
+    });
+    container.appendChild(el);
+  }
+}
+
 function reset() {
   return new Promise(resolve => {
     errorEventTargetNameEl.textContent = '';
     abortEventTargetNameEl.textContent = '';
     errorTransactionErrorEl.textContent = '';
     abortTransactionErrorEl.textContent = '';
+    errorEventErrorEl.textContent = '';
     testResultStatusEl.textContent = '';
     testResultStatusEl.className = 'test-result-status';
 
@@ -79,18 +140,20 @@ function reset() {
   });
 }
 
-function processTransaction(tx, expect) {
+function processTransaction(tx) {
   const result = {
-    errorEventTarget: 'not called',
-    abortEventTarget: 'not called',
-    errorTransactionError: 'not called',
-    abortTransactionError: 'not called',
+    errorEventTarget: NOT_CALLED,
+    abortEventTarget: NOT_CALLED,
+    errorTransactionError: NOT_CALLED,
+    abortTransactionError: NOT_CALLED,
+    errorEventError: NOT_CALLED,
   };
   renderResult(result);
 
   tx.onerror = (ev) => {
     result.errorEventTarget = ev.target.constructor.name;
     result.errorTransactionError = tx.error ? tx.error.toString() : '';
+    result.errorEventError = ev.target.error ? ev.target.error.toString() : '';
     console.log(ev);
   };
   tx.onabort = (ev) => {
@@ -99,30 +162,27 @@ function processTransaction(tx, expect) {
     console.log(ev);
 
     renderResult(result);
-    compareResult(result, expect);
+    evaluateResult(result);
   };
 }
 
 function renderResult(result) {
   errorEventTargetNameEl.textContent = result.errorEventTarget;
   errorTransactionErrorEl.textContent = result.errorTransactionError;
+  errorEventErrorEl.textContent = result.errorEventError;
   abortEventTargetNameEl.textContent = result.abortEventTarget;
   abortTransactionErrorEl.textContent = result.abortTransactionError;
 }
 
-function compareResult(result, expect) {
-  if (expect.errorTransactionError === result.errorTransactionError &&
-      expect.abortTransactionError === result.abortTransactionError &&
-      expect.errorEventTarget === result.errorEventTarget &&
-      expect.abortEventTarget === result.abortEventTarget) {
-    testResultStatusEl.textContent = 'PASS';
-    testResultStatusEl.classList.add('pass');
+function evaluateResult(result) {
+  if ((!result.errorTransactionError ||
+       result.errorEventTarget !== 'IDBTransaction') &&
+      !result.abortTransactionError) {
+    testResultStatusEl.textContent = ABORT_ERROR;
   } else {
-    testResultStatusEl.textContent = 'FAIL';
-    testResultStatusEl.classList.add('fail');
-    console.log(
-        `expect ${JSON.stringify(expect)}, but got ${JSON.stringify(result)}`);
+    testResultStatusEl.textContent = TRANSACTION_ERROR;
   }
+  testResultStatusEl.classList.add('result');
 }
 
 function openDb(upgrade) {
@@ -146,7 +206,7 @@ function runTest(name) {
     });
     openRequest.addEventListener('success', (evt) => {
       const tx = testCase.test(evt.target.result);
-      processTransaction(tx, testCase.expect);
+      processTransaction(tx);
     });
   });
 }
